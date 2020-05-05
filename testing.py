8bc434c3609097e44f6523b809a4d842dec5fd44
xjenny2 <xjenny2@hotmail.com>
2020-05-05 16:35:13 -0400

    Edited to allow for input of a list of motifs to be searched

--- testing.py
+++ testing.py
@@ -1,11 +1,20 @@
 import matchfrequency
+import time
+from itertools import islice
+import csv
 
-# Opening file
-f = open('/Users/jennyxu/Desktop/phospho-files/genes.txt', 'r')
-count = 0
-# Using for loop
-while count < 20:
-    for line in f:
-        count += 1
-        print(line.strip())
-        matchfrequency.find_freq(line.strip())
+with open('/Users/jennyxu/Desktop/phospho-files/genes_edited.txt', 'r') as f, open('/Users/jennyxu/Desktop/phospho-files/results.txt', 'w') as r, open('/Users/jennyxu/Desktop/phospho-files/motifs_test.txt') as p:
+   patternList = list(csv.reader(p, delimiter='\t'))
+   count = 0
+   while count < 30:
+       for line in islice(f, 0, 10):
+            print(line.strip() + ":")
+            # for result in matchfrequency.find_freq(line.strip(), patternList):
+            #     for item in result:
+            #         r.write(str(item))
+            #         r.write("\t")
+            #     r.write("\n")
+            # print("Written to file\n\n")
+       count = count + 2
+       print ("Waiting")
+       time.sleep(10)
